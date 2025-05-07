a5ab1fa4c1aea61cd1907f992cd0159ce3c4c438
Ivo <ivothaielvicencio@gmail.com>
2025-05-07 12:46:09 -0300

    feat: Se crea la pagina y la logica para resolver el problema [Ivo Vicencio]

---
+++ NumeroPrimo/js/evaluacion_primo.js
@@ -0,0 +1,13 @@
+
+export const esPrimo = (numero) => {
+    if (numero <= 1) return false;
+    if (numero === 2) return true;
+    if (numero % 2 === 0) return false;
+    for (let i = 3; i <= Math.sqrt(numero); i += 2) { 
+        //el for sirve para recorrer los números impares desde 3 hasta la raíz cuadrada del número para ver si es primo
+        //si el número es divisible entre alguno de esos números, no es primo
+        // Math.sqrt(numero) es la raíz cuadrada del número
+        if (numero % i === 0) return false;
+    }
+    return true;
+}
